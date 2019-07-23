44ab38b1a1757284bbd45ee6d3774014166d65df
飞哥 <593172440@qq.com>
2019-07-23 22:41:15 +0800

    456

--- SealSystem.BLL/EnterpriseTypeBLL.cs
+++ SealSystem.BLL/EnterpriseTypeBLL.cs
@@ -8,6 +8,20 @@ namespace SealSystem.BLL
 {
     public class EnterpriseTypeBLL
     {
-         
+        /// <summary>
+        /// 添加企业类型
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public async Task CreateEnterpriseType(string name)
+        {
+            using (var db = new DAL.EnterpriseTypeDAL())
+            {
+                await db.AddAsync(new Models.EnterpriseType()
+                {
+                    Name = name
+                });
+            }
+        }
     }
 }
