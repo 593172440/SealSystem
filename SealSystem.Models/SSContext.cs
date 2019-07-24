c0ddfd18b41c231f678fd7c9e8e0e9e4aa45be3d
飞哥 <593172440@qq.com>
2019-07-24 21:53:57 +0800

    实体库v1.1

--- SealSystem.Models/SSContext.cs
+++ SealSystem.Models/SSContext.cs
@@ -7,8 +7,8 @@
 
     public class SSContext : DbContext
     {
-       
-        public SSContext(): base("con1")
+
+        public SSContext() : base("con1")
         {
             Database.SetInitializer<SSContext>(null);
         }
@@ -18,25 +18,69 @@
             modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
             modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
         }
-        
-        public DbSet<User> Users { get; set; }
-        public DbSet<EngravingLevel> EngravingLevels { get; set; }
-        public DbSet<EngravingType> EngravingTypes { get; set; }
-        public DbSet<SealUseUnitInfor> UnitInfors { get; set; }
-        public DbSet<SealUnitClass> EnterpriseClasses { get; set; }
+
+        /// <summary>
+        /// 企业证件类型表(数据上下文)
+        /// </summary>
         public DbSet<EnterpriseDocumentsType> EnterpriseDocumentsTypes { get; set; }
-        public DbSet<SealUnitCategory> SealUnitCategorys { get; set; }
-        public DbSet<Handler> Handlers { get; set; }
-        public DbSet<KeepRecord> keepRecords { get; set; }
-        public DbSet<KeepRecordType> keepRecordTypes { get; set; }
-        public DbSet<RegistrationClass> RegistrationClasses { get; set; }
-        public DbSet<SealInfor> SealInfors { get; set; }
-        public DbSet<SealMaterial> SealMaterials { get; set; }
-        public DbSet<SealShape> SealShapes { get; set; }
-        public DbSet<SealSpecification> SealSpecifications { get; set; }
+        /// <summary>
+        /// 印章审批单位信息表(数据上下文)
+        /// </summary>
+        public DbSet<SealApprovalUnitInfor> SealApprovalUnitInfors { get; set; }
+        /// <summary>
+        /// 印章类型表(登记类别)(数据上下文)
+        /// </summary>
         public DbSet<SealCategory> SealCategorys { get; set; }
+        /// <summary>
+        /// 印章图像信息表(数据上下文)
+        /// </summary>
+        public DbSet<SealImageData> SealImageDatas { get; set; }
+        /// <summary>
+        /// 印章基本信息表(数据上下文)
+        /// </summary>
+        public DbSet<SealInfor> SealInfors { get; set; }
+        /// <summary>
+        /// 印章制作单位信息表(数据上下文)
+        /// </summary>
+        public DbSet<SealMakingUnitInfor> SealMakingUnitInfors { get; set; }
+        /// <summary>
+        /// 章面(体)材料(数据上下文)
+        /// </summary>
+        public DbSet<SealMaterial> SealMaterials { get; set; }
+        /// <summary>
+        /// 印章形状表(数据上下文)
+        /// </summary>
+        public DbSet<SealShape> SealShapes { get; set; }
+        /// <summary>
+        /// 印章状态代码表(数据上下文)
+        /// </summary>
         public DbSet<SealState> SealStates { get; set; }
+        /// <summary>
+        /// 印章使用单位类型表(数据上下文)
+        /// </summary>
+        public DbSet<SealUnitCategory> SealUnitCategorys { get; set; }
+        /// <summary>
+        /// 单位分类表(数据上下文)
+        /// </summary>
+        public DbSet<SealUnitClass> EnterpriseClasses { get; set; }
+        /// <summary>
+        /// 印章使用单位信息表(数据上下文)
+        /// </summary>
+        public DbSet<SealUseUnitInfor> UnitInfors { get; set; }
+        /// <summary>
+        /// 用户表(数据上下文)
+        /// </summary>
+        public DbSet<User> Users { get; set; }
+
+
+
+
+
+
+
+
+
     }
 
-    
+
 }
