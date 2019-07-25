b121ff7abed9ce03bc2bd1e09bb5cf681d27c7da
飞哥 <593172440@qq.com>
2019-07-26 01:01:56 +0800

    表格测试成功

--- SealSystem.Web2/Controllers/SealUnitClassController.cs
+++ SealSystem.Web2/Controllers/SealUnitClassController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,19 +11,39 @@ namespace SealSystem.Web2.Controllers
     public class SealUnitClassController : Controller
     {
         // GET: SealUnitClass
-        public async Task<ActionResult> Index()
+        public ActionResult Index()
         {
             //var data = new BLL.SealUnitClassBLL();
             //await data.RemoveAsync(1);
             //return Content("删除成功");
             return View();
         }
-        public async Task<ActionResult> View1()
+        
+        public ActionResult Test()
         {
-            var data = new BLL.SealUnitClassBLL();
-            var db= await data.GetAllAsync();
-            ViewBag.count = db.Count;
-            return View(db);
+            
+
+            return View();
+        }
+        public string Test2()
+        {
+            Models.DataJson data = new Models.DataJson()
+            {
+                code = 0,
+                msg = "",
+                count = 10,
+                data = new List<Models.Student>()
+                {
+                    new Models.Student(){Id=1,Name="小红",Age=12 },
+                    new Models.Student(){Id=1,Name="小红",Age=12 },
+                    new Models.Student(){Id=1,Name="小红",Age=12 },
+                    new Models.Student(){Id=1,Name="小红",Age=12 },
+                    new Models.Student(){Id=1,Name="小红",Age=12 },
+                    new Models.Student(){Id=1,Name="小红",Age=13 }
+                }.ToArray()
+            };
+            string str = JsonConvert.SerializeObject(data);
+            return str;
         }
     }
 }
