bb84e88b4a786e2c13fe7fc02317f8e6819e3af1
HappyFun <593172440@qq.com>
2019-07-27 17:59:52 +0800

    创建新的项目

---
+++ SealSystem.Web3/Controllers/SealUnitClassesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SealSystem.Models;
+
+namespace SealSystem.Web3.Controllers
+{
+    public class SealUnitClassesController : Controller
+    {
+        private SSContext db = new SSContext();
+
+        // GET: SealUnitClasses
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.EnterpriseClasses.ToListAsync());
+        }
+
+        // GET: SealUnitClasses/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SealUnitClass sealUnitClass = await db.EnterpriseClasses.FindAsync(id);
+            if (sealUnitClass == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sealUnitClass);
+        }
+
+        // GET: SealUnitClasses/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SealUnitClasses/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,CreateTime,IsRemoved")] SealUnitClass sealUnitClass)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EnterpriseClasses.Add(sealUnitClass);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(sealUnitClass);
+        }
+
+        // GET: SealUnitClasses/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SealUnitClass sealUnitClass = await db.EnterpriseClasses.FindAsync(id);
+            if (sealUnitClass == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sealUnitClass);
+        }
+
+        // POST: SealUnitClasses/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,CreateTime,IsRemoved")] SealUnitClass sealUnitClass)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sealUnitClass).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(sealUnitClass);
+        }
+
+        // GET: SealUnitClasses/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SealUnitClass sealUnitClass = await db.EnterpriseClasses.FindAsync(id);
+            if (sealUnitClass == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sealUnitClass);
+        }
+
+        // POST: SealUnitClasses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            SealUnitClass sealUnitClass = await db.EnterpriseClasses.FindAsync(id);
+            db.EnterpriseClasses.Remove(sealUnitClass);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SealSystem.Web3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SealSystem.Web3.MvcApplication" Language="C#" %>
