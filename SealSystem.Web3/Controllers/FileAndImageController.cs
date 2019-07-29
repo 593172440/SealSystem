0eaf1fb5d571407efdf9b5d541d04d9c68ccd247
飞哥 <593172440@qq.com>
2019-07-29 14:14:26 +0800

    123456

---
+++ SealSystem.Web3/Controllers/FileAndImageController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SealSystem.Web3.Controllers
+{
+    public class FileAndImageController : Controller
+    {
+        // GET: FileAndImage
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                string[] imageTypes = { "image/jpeg", "image/png" };
+                if (imageTypes.Any(m => m == file.ContentType))
+                {
+                    string filePath = $"/upLoads/{DateTime.Now.ToString("yyyyMMddhhmmss")}_{file.FileName}";
+                    file.SaveAs(Request.MapPath("~" + filePath));
+                    //return Content(filePath);
+                }
+                //return new HttpStatusCodeResult(500, "格式不正确");
+                return Content("格式不正确");
+                
+            }
+                return View();
+        }
+    }
+}
